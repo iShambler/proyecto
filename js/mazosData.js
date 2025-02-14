ae57807ad766fae47bcb7aba1f14862cc90c2269
Tu Nombre <tu_correo@example.com>
2025-02-14 15:01:43 +0100

    cambios

---
+++ js/mazosData.js
@@ -0,0 +1,181 @@
+const mazosData = {
+  "mazos": [
+    {
+      "nombre": "Murktide Regent",
+      "estrategia": "Control y Tempo",
+      "cartas_clave": [
+        "Murktide Regent",
+        "Dragon's Rage Channeler",
+        "Ragavan, Nimble Pilferer",
+        "Expressive Iteration"
+      ],
+      "imagen": "../img/murk.jpg",
+    },
+    {
+      "nombre": "Death's Shadow",
+      "estrategia": "Aggro-Control basado en pérdida de vida",
+      "cartas_clave": [
+        "Death's Shadow",
+        "Thoughtseize",
+        "Stubborn Denial",
+        "Mishra's Bauble"
+      ],
+      "imagen": "../img/shadow.jpg",
+    },
+    {
+      "nombre": "Living End",
+      "estrategia": "Combo de reanimación con cascada",
+      "cartas_clave": [
+        "Living End",
+        "Violent Outburst",
+        "Shardless Agent",
+        "Street Wraith"
+      ],
+      "imagen": "../img/living.jpg",
+    },
+    {
+      "nombre": "Yawgmoth Combo",
+      "estrategia": "Combo basado en sacrificios y contadores -1/-1",
+      "cartas_clave": [
+        "Yawgmoth, Thran Physician",
+        "Young Wolf",
+        "Strangleroot Geist",
+        "Chord of Calling"
+      ],
+      "imagen": "../img/yawg.jpg",
+    },
+    {
+      "nombre": "4-Color Omnath (Elementals)",
+      "estrategia": "Midrange con cartas de alto valor",
+      "cartas_clave": [
+        "Omnath, Locus of Creation",
+        "Fury",
+        "Solitude",
+        "Risen Reef"
+      ],
+      "imagen": "../img/omnath.jpg",
+    },
+    {
+      "nombre": "Jund",
+      "estrategia": "Midrange con descarte, removal y criaturas eficientes",
+      "cartas_clave": [
+        "Tarmogoyf",
+        "Thoughtseize",
+        "Liliana of the Veil",
+        "Bloodbraid Elf"
+      ],
+      "imagen": "../img/tarmo.jpg",
+    },
+    {
+      "nombre": "Scapeshift",
+      "estrategia": "Combo basado en Valakut y montañas",
+      "cartas_clave": [
+        "Scapeshift",
+        "Valakut, the Molten Pinnacle",
+        "Primeval Titan",
+        "Sakura-Tribe Elder"
+      ],
+      "imagen": "../img/scape.jpg",
+    },
+    {
+      "nombre": "Burn",
+      "estrategia": "Daño directo al oponente con hechizos rápidos y criaturas agresivas",
+      "cartas_clave": [
+        "Goblin Guide",
+        "Monastery Swiftspear",
+        "Lightning Bolt",
+        "Eidolon of the Great Revel"
+      ],
+      "imagen": "../img/goblin.jpg",
+    },
+    {
+      "nombre": "Affinity (Hardened Scales)",
+      "estrategia": "Sinergias de artefactos y contadores +1/+1",
+      "cartas_clave": [
+        "Arcbound Ravager",
+        "Hardened Scales",
+        "Walking Ballista",
+        "Steel Overseer"
+      ],
+      "imagen": "../img/opal.jpg",
+    },
+    {
+      "nombre": "Amulet Titan",
+      "estrategia": "Combo de tierras aceleradas con Primeval Titan",
+      "cartas_clave": [
+        "Amulet of Vigor",
+        "Primeval Titan",
+        "Summoner's Pact",
+        "Simic Growth Chamber"
+      ],
+      "imagen": "../img/titan.jpg",
+    },
+    {
+      "nombre": "Grixis Shadow",
+      "estrategia": "Aggro-control con Death's Shadow y descarte eficiente",
+      "cartas_clave": [
+        "Death's Shadow",
+        "Thoughtseize",
+        "Snapcaster Mage",
+        "Kolaghan's Command"
+      ],
+      "imagen": "../img/shadow.jpg",
+    },
+    {
+      "nombre": "UW Control",
+      "estrategia": "Control tradicional con contrahechizos y removal masivo",
+      "cartas_clave": [
+        "Teferi, Hero of Dominaria",
+        "Supreme Verdict",
+        "Cryptic Command",
+        "Snapcaster Mage"
+      ],
+      "imagen": "../img/soli.jpg",
+    },
+    {
+      "nombre": "Tron (Mono-Green)",
+      "estrategia": "Ramp para lanzar amenazas grandes rápidamente usando tierras UrzaTron",
+      "cartas_clave": [
+        "Karn Liberated",
+        "Ulamog, the Ceaseless Hunger",
+        "Wurmcoil Engine",
+        "Sylvan Scrying"
+      ],
+      "imagen": "../img/karn.jpg",
+    },
+    {
+      "nombre": "Infect",
+      "estrategia": "Mazo aggro-combo que busca ganar rápidamente con daño infectado",
+      "cartas_clave": [
+        "Blighted Agent",
+        "Glistener Elf",
+        "Inkmoth Nexus",
+        "Vines of Vastwood"
+      ],
+      "imagen": "../img/infect.jpg",
+    },
+    {
+      "nombre": "Domain Zoo",
+      "estrategia": "Agresivo multicolor aprovechando el mecanismo del Dominio",
+      "cartas_clave": [
+        "Ragavan, Nimble Pilferer",
+        "Lightning Bolt",
+        "Tribal Flames",
+        "Thoughtseize"
+      ],
+      "imagen": "../img/draco.jpg",
+    },
+    {
+      "nombre": "Boros Energy",
+      "estrategia": "Mazo agresivo basado en energía",
+      "cartas_clave": [
+        "Aetherworks Marvel",
+        "Bomat Courier",
+        "Embercleave",
+        "Light Up the Stage",
+        "Chandra, Torch of Defiance"
+      ],
+      "imagen": "../img/phlage.jpg "
+    }
+  ]
+}
